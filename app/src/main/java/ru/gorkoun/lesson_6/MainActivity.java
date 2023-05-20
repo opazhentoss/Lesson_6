57b4c3d0b1dfa25bb58b1162b7d6605a6581f384
evgen <gorkoune@gmail.com>
2023-05-20 16:54:26 +0300

    Initial commit

---
+++ app/src/main/java/ru/gorkoun/lesson_6/MainActivity.java
@@ -0,0 +1,16 @@
+package ru.gorkoun.lesson_6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lesson_6.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
